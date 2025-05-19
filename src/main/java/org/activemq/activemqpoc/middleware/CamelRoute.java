bd53daab6f57dbc0cf42291b86ad41e8ab23bc43
IgorArthur <igorart1210@gmail.com>
2025-05-19 16:44:46 -0300

    Simulates Feature Flag for setting messageStatus

--- src/main/java/org/activemq/activemqpoc/middleware/CamelRoute.java
+++ src/main/java/org/activemq/activemqpoc/middleware/CamelRoute.java
@@ -13,7 +13,11 @@ public class CamelRoute extends RouteBuilder {
         from("activemq:queue:activemqpoc-input-queue")
                 .process(exchange -> {
                     OrderObject orderObject = exchange.getIn().getBody(OrderObject.class);
-                    orderObject.setMessageStatus("PROCESSED");
+
+                    // Simulate a Feature Flag with a 50 % chance for each status
+                    String status = Math.random() < 0.5 ? "PROCESSED" : "ERROR";
+                    orderObject.setMessageStatus(status);
+
                     exchange.getIn().setBody(orderObject);
                 })
                 .to("activemq:queue:activemqpoc-output-queue");
